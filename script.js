05589d9a4fb076068a5285f9f28b913191c57387
r.a.dafin <165747132+dafinffx@users.noreply.github.com>
2024-11-04 22:10:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function onGoogleSignIn(googleUser) {
-    const profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
 
-    const id_token = googleUser.getAuthResponse().id_token;
-    console.log('ID Token: ' + id_token);
-    // Send the ID token to your server to validate the user
-
-    // Display the profile image
-    const profileImage = document.getElementById('profileImage');
-    profileImage.src = profile.getImageUrl();
-    profileImage.classList.remove('d-none');
-}
